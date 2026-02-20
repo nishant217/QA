f09ba1c3382ae3b99094f681a98c66ea30fc7e33
Nishant_Sharma <ns902709@gmail.com>
2026-02-20 13:58:55 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
 // Initialize theme BEFORE React renders to prevent flash
 const initTheme = () => {
   const stored = localStorage.getItem('nyneos-theme');
